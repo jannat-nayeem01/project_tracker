138882b551341b4a1c6bd1ed22e5c1593f7f65f5
Jannatun Nayeem <jannat.nayeem01@gmail.com>
2023-11-01 14:54:25 +0700

    student steps added

---
+++ features/step_definitions/student_steps.rb
@@ -0,0 +1,29 @@
+
+Given ('I am a teacher') do
+    
+end 
+
+Given('there is a project') do
+
+end
+Given('I want to add a student to the project') do
+
+end
+Given('I am signed in') do
+
+end    
+When('I visit the projects page') do
+
+end  
+Then('I should see a link for the project When I click the link for the project') do
+
+end
+Then('I should see the details of my project And I should see a form to add a student When I submit the form') do
+
+end
+Then('I should see the details of my project') do
+
+end 
+Then('I should see the student added to the project') do
+
+end  
